4bb044310a5db8fdb2a7eab4c249e8472658fcb9
Kemal Marić <32041545+Kemovizor@users.noreply.github.com>
2021-11-30 17:39:09 +0100

    Zadatak 1.X16 fix
    ...

--- Zadaci 1.X/Rješenja/Zadatak 1.X16.cpp
+++ Zadaci 1.X/Rješenja/Zadatak 1.X16.cpp
@@ -13,26 +13,11 @@ bool isSlovo(char c) {
 	return isUpper(c) || isLower(c);
 }
 char uradiPomak(char c, int pomak) {
-	int donjaGranica = 'A', gornjaGranica = 'Z' +1;
-	pomak = pomak % (gornjaGranica - donjaGranica);
-	// na ovaj se nacin pomak modulira (%) duzinom alfabeta (ukupnim brojem slova)
-	//tako se obezbjedjuje da vrijednost pomaka bude u okviru duzine alfabeta 
-	//Npr: neka je duzina engleskog alfabeta 26 (gornjaGranica-donjaGranica)
-	//  0 % 26 = 0  (konacni pomak = 0)
-	//  1 % 26 = 1  (konacni pomak = 1)
-	//  2 % 26 = 2  (konacni pomak = 2)
-	//  3 % 26 = 3  (konacni pomak = 3)
-	// ...
-	// 24 % 26 = 24 (konacni pomak = 24)
-	// 25 % 26 = 25 (konacni pomak = 25)
-	// 26 % 26 = 0 (konacni pomak = 0)
-	// 27 % 26 = 1 (konacni pomak = 1)
-	// 28 % 26 = 2 (konacni pomak = 2)
-	// 29 % 26 = 3 (konacni pomak = 3)
-	// itd.
-	//Ovo je takozvano cirkularno kretanje (npr. kretanje kazaljke na satu (od 0 do 23 i tako u krug))
-	//Primjer kazaljke na satu se takodjer moze simulirati upotrebom modularne aritmetike
-	return char(c + pomak);
+	
+	char prvoSlovo = isUpper(c) ? 'A' : 'a';
+	int ascii = c + pomak;
+	ascii = ((ascii - prvoSlovo) % 26) + prvoSlovo;
+	return char(ascii);
 }
 void main()
 {
@@ -46,6 +31,5 @@ void main()
 		cout << "Unesite c: " << endl;
 		cin >> c;
 	} while (!isSlovo(c));
-
 	cout << "Nakon pomicanja za vrijednost " << n << " dobija se slovo " << uradiPomak(c, n) << endl;
 }
